8388cdf851eeb4ebd2581df09a4cb539cea2b609
ie2030 <ielongford@gmail.com>
2017-03-16 17:48:28 +0000

    Initial Structure of Project

---
+++ src/App.js
@@ -0,0 +1,53 @@
+  import React from 'react';
+
+    var SelectBox = React.createClass({
+      render: function(){
+           return (
+             <div className="col-md-10">
+            <input type="text" placeholder="Search" />
+            Sort by:
+            <select>
+              <option value="name">Alphabetical</option>
+              <option value="age">Newest</option>
+            </select>
+             </div>
+            );
+          }
+       });
+
+
+        // TODO (missing component)
+
+       var Article = React.createClass({
+            render: function(){
+                var displayedPhones = this.props.phones.map(function(phone) {
+                  return <PhoneItem key={phone.id} phone={phone} /> ;
+                }) ;
+                return (
+                        <div className="col-md-10">
+                          <ul className="phones">
+                              {displayedPhones}
+                          </ul>
+                        </div>
+                  ) ;
+            }
+        });
+
+    var NewsApp = React.createClass({
+      render: function(){
+          return (
+              <div className="view-container">
+              <div className="view-frame">
+                 <div className="container-fluid">
+                   <div className="row">
+                       <SelectBox />
+                       {/* TODO */}
+                  </div> 
+                  </div>                   
+                </div>
+              </div>
+          );
+      }
+    });
+
+    export default NewsApp;
