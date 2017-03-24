0105c65ddc850d14d11b7c3d8a40d0062f231295
ie2030 <ielongford@gmal.com>
2017-03-24 12:57:44 +0000

    Added onClick function

--- src/App.js
+++ src/App.js
@@ -18,70 +18,84 @@
 
 
     var Article = React.createClass({
-      
-      propTypes: {
-      data: React.PropTypes.shape({
-        author: React.PropTypes.string.isRequired,
-        text: React.PropTypes.string.isRequired
-      })
-     },
 
-     render:function(){
+      propTypes: {
+        data: React.PropTypes.shape({
+          author: React.PropTypes.string.isRequired,
+          text: React.PropTypes.string.isRequired,
+          bigText: React.PropTypes.string.isRequired
+        })
+      },
+
+      getInitialState: function() {
+        return {
+          visible: false
+        };
+      },
+
+      readmoreClick: function(e) {
+        e.preventDefault();
+        this.setState({visible: true});
+      },
+
+      render:function(){
     var author = this.props.data.author,  //  added immutable data  = added props in render 
     text = this.props.data.text,
-    bigText = this.props.data.bigText;
+    bigText = this.props.data.bigText,
+    visible = this.state.visible;
 
     return(
       <div className='article'>
       <p className='news__author'>{author}:</p>
       <p className='news__text'>{text}:</p>
-      <p className='news_bigText'>{bigText}:</p>
-      </div>
-      )
+    <a href="#" onClick={this.readmoreClick} className={'news__readmore ' + (visible ? 'none': '')}>Details</a> {/*for readmore link if visible === true do not show link*/}
+    <p className={'news__bigText ' + (visible ? '': 'none')}>{bigText}</p>
+    </div>
+    )
   }
 });
 
-    var News = React.createClass({
-      propTypes: {
-        data: React.PropTypes.array.isRequired
-      },
+var News = React.createClass({
+  propTypes: {
+    data: React.PropTypes.array.isRequired
+  },
 
-      render:function(){
-        var data = this.props.data;
-        var newsTemplate;
+  render:function(){
+    var data = this.props.data;
+    var newsTemplate;
 
-        if (data.length > 0) {
-          newsTemplate = data.map(function(item, index) { 
-    // Array method map = created new array and dispaly elements. 
-    //Save array in var newsTemplate 
-    return (
-      <div key={index}>                             
-      <Article data={item} />
-      </div>
-      )
-  })
-        } else {
-          newsTemplate = <p>No news</p>
-        }
-
+    if (data.length > 0) {
+      newsTemplate = data.map(function(item, index) { 
+        // Array method map = created new array and dispaly elements. 
+        //Save array in var newsTemplate 
         return (
-        <div className="news"> 
-        {newsTemplate}
-        <strong className={'news__count ' + (data.length > 0 ? '':'none') }>Total news: {data.length}</strong>
+        <div key={index}>                             
+        <Article data={item} />
         </div>
-        );
-      }
-    });
-
-
-    var App = React.createClass({
-      render: function() {
-        return (
-        <div className="app">
-        <h3>News</h3>
-      <News data = {this.props.my_news}/>  {/* added data attributes in components*/}
-      </div>
-      );
+        )
+      })
+    } else {
+      newsTemplate = <p>No news</p>
     }
-  });
-  export default App;
+
+    return (
+    <div className="news"> 
+    {newsTemplate}
+    <strong className={'news__count ' + (data.length > 0 ? '':'none') }>Total news: {data.length}</strong>
+    </div>
+    );
+  }
+});
+
+
+var App = React.createClass({
+  render: function() {
+    return (
+    <div className="app">
+    <h3>News</h3>
+  <News data = {this.props.my_news}/>  {/* added data attributes in components*/}
+  </div>
+  );
+}
+});
+export default App; 
